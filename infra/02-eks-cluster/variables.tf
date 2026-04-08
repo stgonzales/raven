5cf1ce4b392a783b2ab35574dbd17743f99de3d2
Stephen Goncalves <stiefo.o@gmail.com>
2026-04-08 17:53:23 +0100

    Add ECR repository configuration and introduce new variable for ECR repositories in Terraform setup

--- infra/02-eks-cluster/variables.tf
+++ infra/02-eks-cluster/variables.tf
@@ -25,17 +25,39 @@ variable "aws_role" {
 
 variable "eks_cluster" {
   type = object({
-    name            = string
-    role_name       = string
-    node_group_name = string
+    name                 = string
+    role_name            = string
+    node_group_name      = string
     node_group_role_name = string
   })
   description = "The EKS cluster configuration"
 
   default = {
-    name            = "raven-eks-cluster"
-    role_name       = "raven-eks-cluster-role"
-    node_group_name = "raven-eks-node-group"
+    name                 = "raven-eks-cluster"
+    role_name            = "raven-eks-cluster-role"
+    node_group_name      = "raven-eks-node-group"
     node_group_role_name = "raven-eks-node-group-role"
   }
+}
+
+variable "ecr_repositories" {
+  type = list(object({
+    name                 = string
+    image_tag_mutability = string
+    force_delete         = bool
+  }))
+  description = "The ECR repositories configuration"
+
+  default = [
+    {
+      name                 = "raven-ecr-repository/production/backend"
+      image_tag_mutability = "MUTABLE"
+      force_delete         = true
+    },
+    {
+      name                 = "raven-ecr-repository/production/frontend"
+      image_tag_mutability = "MUTABLE"
+      force_delete         = true
+    },
+  ]
 }
